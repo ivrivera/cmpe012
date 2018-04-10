c4cb570d3dc335ad6e12c669cdeb989fed1fe20c
ivrivera <ivrivera@ucsc.edu>
2018-04-10 10:03:22 -0700

    added FeedBabe in the Lab1 Folder

---
+++ lab1/FeedBabe.java
@@ -0,0 +1,14 @@
+//----------------------
+//Ivanna Rivera
+//CMPE 12
+//Lab 1
+//April 9th,2018
+//FeedBabe.java
+//----------------------
+import java.io.*;
+import java.util.Scanner;
+
+class FeedBabe{
+  public static void main(String[] args){
+	
+
