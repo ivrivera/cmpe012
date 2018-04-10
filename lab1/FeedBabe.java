bd29283c8190d56259d6c0128d5af767b4a4b71d
ivrivera <ivrivera@ucsc.edu>
2018-04-10 10:34:24 -0700

    test running if statement

--- lab1/FeedBabe.java
+++ lab1/FeedBabe.java
@@ -8,7 +8,15 @@
 
 class FeedBabe{
   public static void main(String[] args){
-	int n;
+	int n = 500;
+	char divisible;
+
+	for (int i = 0; i < n; i++)
+	 	if(n %3){
+			System.out.println("FEED);
+		}
+  }
+}
 	
 	
 
