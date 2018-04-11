c7842590493e1dc46fb346f263a881268f90b73c
ivrivera <ivrivera@ucsc.edu>
2018-04-10 21:35:58 -0700

    fixed n to match i

--- lab1/FeedBabe.java
+++ lab1/FeedBabe.java
@@ -9,14 +9,18 @@
 class FeedBabe{
   public static void main(String[] args){
 	int n = 500;
-	char divisible;
 
 	for (int i = 1; i <= n; i++){
-		if(n %3 == 0){
+		if(i %3 == 0)
+		{
 		System.out.println("FEED");
-  		} else if(n %4 == 0){
-		System.out.println("BABE");
-		} else if(n %3 == 0 && n %4 == 0){
+  		}
+		else if(i %4 == 0)
+		{
+		System.out.println("BABE"); 
+		}
+		if(i %3 == 0 && i %4 == 0)
+		{
 		System.out.println("FEEDBABE");
 		}
 	}
